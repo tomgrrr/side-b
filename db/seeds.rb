51517c2a6c1998855b3e13f522d531923d04b8de
vitorcastro <v170rc@hotmail.com>
2025-12-02 12:41:47 +0100

    apitest

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,38 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require "json"
+require "open-uri"
+
+BASE = "https://api.discogs.com"
+KEY    = ENV["DISCOGS_KEY"]
+SECRET = ENV["DISCOGS_SECRET"]
+
+#50.times do |i|
+
+  #begin
+    #url="#{BASE}/artists/#{i+5000}/releases?key=#{KEY}&secret=#{SECRET}"
+
+    #user_serialized = URI.parse(url).read
+    #user = JSON.parse(user_serialized)
+    #Artist.create(name: user["name"])
+    #puts user
+  #rescue
+    #puts "error with id:#{i}"
+  #end
+#end
+
+
+url="#{BASE}/artists/15885/releases?key=#{KEY}&secret=#{SECRET}"
+
+#url= "https://api.discogs.com/database/search?q=nirvana?key=#{KEY}&secret=#{SECRET}"
+
+
+
+    user_serialized = URI.parse(url).read
+    user = JSON.parse(user_serialized)
+    Artist.create(name: user["name"])
+    puts Artist.last
+
+    Vinyl.create({
+      name: user["title"],
+      release_date: user["year"],
+
+    })
