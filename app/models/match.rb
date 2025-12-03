f3faaca92bd5a11a4b6278fb3d31d19007d2b84a
Bassam Renaud <brenaud150@gmail.com>
2025-12-03 21:22:15 +0100

    almost finished

--- app/models/match.rb
+++ app/models/match.rb
@@ -2,4 +2,14 @@ class Match < ApplicationRecord
   belongs_to :vinyl
   belongs_to :user
   belongs_to :playlist, optional: true
+
+  def total_value(array)
+    value = 0
+
+    array.each do |s|
+     value += s.vinyl.price
+    end
+
+    value
+end
 end
