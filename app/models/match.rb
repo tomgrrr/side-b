eb2eda565e718136b11df47972860e77073bf6b6
Bassam Renaud <brenaud150@gmail.com>
2025-12-04 17:40:26 +0100

    test

--- app/models/match.rb
+++ app/models/match.rb
@@ -3,6 +3,15 @@ class Match < ApplicationRecord
   belongs_to :user
   belongs_to :playlist, optional: true
 
+
+  def total_value(array)
+    total = 0
+    array.each do |a|
+      total = a.vinyl.price
+    end
+    total
+  end
+
   def user_taste(matches)
     titles = ""
     genres = ""
