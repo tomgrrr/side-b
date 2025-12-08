f99228e57acf189a15c059b4e863173736882eae
Bassam Renaud <brenaud150@gmail.com>
2025-12-08 17:20:05 +0100

    t

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -8,11 +8,15 @@ class PlaylistsController < ApplicationController
     @playlist = Playlist.find(params[:id])
   end
 
+  def new
+    @playlist = Playlist.new
+  end
+
   def create
     @playlist = Playlist.new(playlist_params)
 
     if @playlist.save
-      redirect_back fallback_location: root_path, notice: "Playlist créée avec succès!"
+      redirect_to playlist_path(@playlist), notice: "Playlist créée avec succès!"
     else
       redirect_back fallback_location: root_path, alert: "Erreur lors de la création de la playlist"
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_12_08_111352) do
+ActiveRecord::Schema[7.1].define(version: 2025_12_08_150226) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "vector"
@@ -109,7 +109,6 @@ ActiveRecord::Schema[7.1].define(version: 2025_12_08_111352) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "image"
   end
 
   create_table "solid_queue_blocked_executions", force: :cascade do |t|
