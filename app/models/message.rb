1269b4e09ee366c7ac7959351984b944da353483
Grenié Tom <tom.grenie@gmail.com>
2025-12-09 14:49:01 +0100

    Continue turbo

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,13 +1,5 @@
 class Message < ApplicationRecord
   belongs_to :chat
   has_one_attached :file
-  after_create_commit :broadcast_append_to_chat
-
-  private
-
-
-def broadcast_append_to_chat
-  broadcast_append_to chat, target: "messages", partial: "messages/message", locals: { message: self }
-end
 
 end
